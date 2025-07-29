fc544dd1f0f34ed95f444d1cbe2634817bf90bb1
heisdanielade <heisdanielade@gmail.com>
2025-07-29 15:55:31 +0200

    update: setup jwt auth with user checks dependency
    -> dependencies.py for get_current_user & check_enabled_user

---
+++ app/api/dependencies.py
@@ -0,0 +1,30 @@
+# app/api/dependencies.py
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlmodel import Session
+from app.db.session import get_session
+from app.models.user import User
+from app.core.jwt import decode_access_token
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+
+def get_current_user(token: str = Depends(oauth2_scheme), session: Session = Depends(get_session)) -> User:
+    payload = decode_access_token(token)
+    user_id = payload.get("sub")
+    if not user_id:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Invalid authentication credentials")
+    user: User = session.get(User, int(user_id))  # type: ignore
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
+    return user
+
+
+def check_enabled_user(user: User = Depends(get_current_user)):
+    if not user.enabled:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="User account not enabled")
+    return user
---
+++ app/core/jwt.py
@@ -0,0 +1,32 @@
+# app/core/jwt.py
+
+from datetime import datetime, timezone, timedelta
+from typing import Any, Union
+
+from jose import JWTError, jwt
+from fastapi import HTTPException, status
+
+from app.core.config import settings
+
+ALGORITHM = "HS256"
+
+
+def decode_access_token(token: str) -> dict[str, Any]:
+    try:
+        payload = jwt.decode(token, settings.JWT_SECRET_KEY,  # type: ignore
+                             algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials"
+        )
+
+
+def create_access_token(data: dict[str, Union[str, int]]):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + \
+        timedelta(seconds=settings.JWT_EXPIRATION_TIME)
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=ALGORITHM)
