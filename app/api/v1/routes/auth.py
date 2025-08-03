efabc06e3e83e57153e3235e769fb25a61e7856b
heisdanielade <heisdanielade@gmail.com>
2025-08-03 20:58:04 +0200

    update: added password reset logic and routes     -> Request password reset email     -> Reset user password

--- app/api/v1/routes/auth.py
+++ app/api/v1/routes/auth.py
@@ -6,9 +6,9 @@ from fastapi import APIRouter, Depends, status
 from sqlmodel import Session
 
 from app.db.session import get_session
-from app.schemas.user import UserCreate
-from app.schemas.auth import VerifyEmailRequest, LoginRequest, ResendVerificationEmailRequest
-from app.services.auth_service import register_new_user, verify_new_user_email, login_existing_user, resend_verification_email_to_user
+import app.schemas.auth as auth_schemas
+import app.schemas.user as user_schemas
+from app.services.auth_service import AuthService
 from app.utils.response import standard_response
 
 
@@ -16,7 +16,7 @@ router = APIRouter()
 
 
 @router.post("/register", status_code=status.HTTP_201_CREATED)
-async def register(user_create: UserCreate, db: Session = Depends(get_session)) -> dict[str, Any]:
+async def register(user_create: user_schemas.UserCreate, db: Session = Depends(get_session)) -> dict[str, Any]:
     """
     Register a new user account.
 
@@ -33,7 +33,7 @@ async def register(user_create: UserCreate, db: Session = Depends(get_session)) 
         HTTPException: 409 Conflict if the email is already registered.
     """
 
-    await register_new_user(user_create=user_create, db=db)
+    await AuthService.register_new_user(user_create=user_create, db=db)
 
     return standard_response(
         status="success",
@@ -42,7 +42,7 @@ async def register(user_create: UserCreate, db: Session = Depends(get_session)) 
 
 
 @router.post("/verify-email", status_code=status.HTTP_200_OK)
-async def verify_email(user_verify: VerifyEmailRequest, db: Session = Depends(get_session)) -> dict[str, Any]:
+async def verify_email(user_verify: auth_schemas.VerifyEmailRequest, db: Session = Depends(get_session)) -> dict[str, Any]:
     """
     Endpoint to verify a user's email address.
 
@@ -61,13 +61,13 @@ async def verify_email(user_verify: VerifyEmailRequest, db: Session = Depends(ge
         HTTPException: 409 Conflict if the account is already verified.
         HTTPException: 400 Bad Request if the verification code is invalid or expired.
     """
-    await verify_new_user_email(user_verify=user_verify, db=db)
+    await AuthService.verify_user_email(user_verify=user_verify, db=db)
 
     return standard_response(status="success", message="Email verified successfully")
 
 
 @router.post("/login", status_code=status.HTTP_200_OK)
-async def login(data: LoginRequest, db: Session = Depends(get_session)) -> dict[str, Any]:
+async def login(data: auth_schemas.LoginRequest, db: Session = Depends(get_session)) -> dict[str, Any]:
     """
     Authenticate a user and issue an access token.
 
@@ -83,7 +83,7 @@ async def login(data: LoginRequest, db: Session = Depends(get_session)) -> dict[
         HTTPException: 401 Unauthorized if login credentials are invalid.
         HTTPException: 403 Forbidden if the user account is unverified.
     """
-    response = await login_existing_user(login_request=data, db=db)
+    response = await AuthService.login_existing_user(login_request=data, db=db)
 
     return standard_response(
         status="success",
@@ -93,7 +93,7 @@ async def login(data: LoginRequest, db: Session = Depends(get_session)) -> dict[
 
 
 @router.post("/resend-verification", status_code=status.HTTP_200_OK)
-async def resend_verification_email(request: ResendVerificationEmailRequest, db: Session = Depends(get_session)) -> dict[str, Any]:
+async def resend_verification_email(request: auth_schemas.ResendVerificationEmailRequest, db: Session = Depends(get_session)) -> dict[str, Any]:
     """
     Resend a verification email to a user.
 
@@ -105,9 +105,51 @@ async def resend_verification_email(request: ResendVerificationEmailRequest, db:
     Returns:
         dict(str, Any): Success message indicating that the verification email has been sent.
     """
-    await resend_verification_email_to_user(request=request, db=db)
+    await AuthService.resend_verification_email(request=request, db=db)
 
     return standard_response(
         status="success",
         message="Verification email sent successfully"
+    )
+
+
+@router.post("request-password-reset", status_code=status.HTTP_200_OK)
+async def request_password_reset(request: auth_schemas.PasswordResetLinkRequest, db: Session = Depends(get_session)) -> dict[str, Any]:
+    """
+    Resend a password reset email to a user.
+
+    Accepts the user's email and triggers sending of an email with password reset instructions.
+
+    Args:
+        request (PasswordResetLinkRequest): Email address to send the reset email to.
+
+    Returns:
+        dict(str, Any): Success message indicating that the password reset email has been sent.
+    """
+    await AuthService.request_password_reset(request=request, db=db)
+
+    return standard_response(
+        status="success",
+        message="Password reset email sent successfully"
+    )
+
+
+@router.post("reset-password", status_code=status.HTTP_200_OK)
+async def reset_password(request: auth_schemas.ResetPasswordRequest, db: Session = Depends(get_session)) -> dict[str, Any]:
+    """
+    Change the password of a user.
+
+    Accepts the reset token and new password then changes the user's password if the token is valid and user exists.
+
+    Args:
+        request (ResetPasswordRequest): A JWT for password reset and the new user's password.
+
+    Returns:
+        dict(str, Any): Success message indicating that the user's password has been reset.
+    """
+    await AuthService.reset_user_password(token=request.token, new_password=request.new_password, db=db)
+
+    return standard_response(
+        status="success",
+        message="Password reset successfully"
     )
--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -9,180 +9,233 @@ from fastapi import HTTPException, status
 from sqlmodel import Session, select
 
 from app.models.user import User
-from app.schemas.user import UserCreate
-from app.schemas.auth import VerifyEmailRequest, LoginRequest, ResendVerificationEmailRequest
 from app.core.security import hash_password, verify_password
-from app.services import email_service as es
-from app.core.jwt import create_access_token
 from app.core.config import settings
+import app.schemas.auth as auth_schemas
+import app.schemas.user as user_schemas
+from app.services.email_service import EmailService
+from app.core.jwt import create_access_token, create_password_reset_token, decode_token
 
 
-def generate_verification_code(length: int = 6):
-    return ''.join(random.choices(string.digits, k=length))
+class AuthService:
+    @staticmethod
+    def generate_verification_code(length: int = 6):
+        return ''.join(random.choices(string.digits, k=length))
 
+    @staticmethod
+    async def register_new_user(user_create: user_schemas.UserCreate, db: Session) -> None:
+        """
+        Register a new user in the database and initiate email verification.
 
-async def register_new_user(user_create: UserCreate, db: Session) -> None:
-    """
-    Register a new user in the database and initiate email verification.
+        Checks if a user with the provided email already exists. If not, creates a new user record
+        with a hashed password and a verification code valid for a limited time. Sends a verification
+        email containing the code to the user's email address.
 
-    Checks if a user with the provided email already exists. If not, creates a new user record
-    with a hashed password and a verification code valid for a limited time. Sends a verification
-    email containing the code to the user's email address.
+        Args:
+            user_create (UserCreate): Pydantic model containing the user's email and password.
+            db (Session): SQLModel session used to perform database operations.
 
-    Args:
-        user_create (UserCreate): Pydantic model containing the user's email and password.
-        db (Session): SQLModel session used to perform database operations.
+        Raises:
+            HTTPException: 409 Conflict if a user with the given email already exists.
+        """
+        # Check if user already exists
+        existing_user = db.exec(select(User).where(
+            User.email == user_create.email)).first()
+        if existing_user:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                                detail="Account already exists")
 
-    Raises:
-        HTTPException: 409 Conflict if a user with the given email already exists.
-    """
-    # Check if user already exists
-    existing_user = db.exec(select(User).where(
-        User.email == user_create.email)).first()
-    if existing_user:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                            detail="Account already exists")
+        # Verification code to be sent yo user email to enable user's account
+        verification_code = AuthService.generate_verification_code()
+        expires_at = datetime.now(timezone.utc) + \
+            timedelta(minutes=10)  # Valid for 10 minutes
 
-    # Verification code to be sent yo user email to enable user's account
-    verification_code = generate_verification_code()
-    expires_at = datetime.now(timezone.utc) + \
-        timedelta(minutes=10)  # Valid for 10 minutes
+        user: User = User(
+            email=user_create.email,
+            hashed_password=hash_password(user_create.password),
+            verification_code=verification_code,
+            verification_code_expires_at=expires_at,
+            created_at=datetime.now(timezone.utc),
+            updated_at=datetime.now(timezone.utc)
+        )
+        db.add(user)
+        db.commit()
+        db.refresh(user)
 
-    user: User = User(
-        email=user_create.email,
-        hashed_password=hash_password(user_create.password),
-        verification_code=verification_code,
-        verification_code_expires_at=expires_at,
-        created_at=datetime.now(timezone.utc),
-        updated_at=datetime.now(timezone.utc)
-    )
+        await EmailService.send_verification_email(email_to=user.email, code=verification_code)
 
-    db.add(user)
-    db.commit()
-    db.refresh(user)
+    @staticmethod
+    async def verify_user_email(user_verify: auth_schemas.VerifyEmailRequest, db: Session) -> None:
+        """
+        Verify a user's email address using a verification code.
 
-    await es.send_verification_email(email_to=user.email, code=verification_code)
+        Checks if the user exists and is not already verified. Validates that the
+        provided verification code matches and is not expired. Upon successful
+        verification, marks the user as verified and sends a welcome email.
 
+        Args:
+            user_verify (VerifyEmailRequest): Pydantic model containing user's email and verification code.
+            db (Session): SQLModel session for database operations.
 
-async def verify_new_user_email(user_verify: VerifyEmailRequest, db: Session) -> None:
-    """
-    Verify a user's email address using a verification code.
+        Raises:
+            HTTPException: 404 Not Found if the user account does not exist.
+            HTTPException: 409 Conflict if the account is already verified.
+            HTTPException: 400 Bad Request if the verification code is invalid or expired.
+        """
+        stmt = select(User).where(User.email == user_verify.email)
+        user = db.exec(stmt).one_or_none()
 
-    Checks if the user exists and is not already verified. Validates that the
-    provided verification code matches and is not expired. Upon successful
-    verification, marks the user as verified and sends a welcome email.
+        if not user:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail="Account does not exist")
 
-    Args:
-        user_verify (VerifyEmailRequest): Pydantic model containing user's email and verification code.
-        db (Session): SQLModel session for database operations.
+        if user.is_verified:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                                detail="Account already verified")
 
-    Raises:
-        HTTPException: 404 Not Found if the user account does not exist.
-        HTTPException: 409 Conflict if the account is already verified.
-        HTTPException: 400 Bad Request if the verification code is invalid or expired.
-    """
-    stmt = select(User).where(User.email == user_verify.email)
-    user = db.exec(stmt).one_or_none()
+        # Make expires_at timezone-aware
+        expires_at = user.verification_code_expires_at
+        if expires_at and expires_at.tzinfo is None:
+            expires_at = expires_at.replace(tzinfo=timezone.utc)
 
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail="Account does not exist")
+        # Check code matches and is not expired
+        if (user.verification_code != user_verify.verification_code or not user.verification_code_expires_at or expires_at < datetime.now(timezone.utc)):  # type: ignore
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail="Invalid or expired verification code")
 
-    if user.is_verified:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                            detail="Account already verified")
+        user.is_verified = True
+        user.verification_code = None
+        user.verification_code_expires_at = None
 
-    # Make expires_at timezone-aware
-    expires_at = user.verification_code_expires_at
-    if expires_at and expires_at.tzinfo is None:
-        expires_at = expires_at.replace(tzinfo=timezone.utc)
+        db.add(user)
+        db.commit()
 
-    # Check code matches and is not expired
-    if (user.verification_code != user_verify.verification_code or not user.verification_code_expires_at or expires_at < datetime.now(timezone.utc)):  # type: ignore
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail="Invalid or expired verification code")
+        await EmailService.send_welcome_email(email_to=user.email)
 
-    user.is_verified = True
-    user.verification_code = None
-    user.verification_code_expires_at = None
+    @staticmethod
+    async def login_existing_user(login_request: auth_schemas.LoginRequest, db: Session) -> dict[str, Any]:
+        """
+        Authenticate an existing user and generate a JWT access token.
 
-    db.add(user)
-    db.commit()
+        Queries the database for a user matching the provided email. Verifies the provided password
+        against the stored hashed password. Checks if the user account is verified. If authentication
+        is successful, generates a JWT token with an expiration time.
 
-    await es.send_welcome_email(email_to=user.email)
+        Args:
+            login_request (LoginRequest): Pydantic model containing user email and password.
+            db (Session): SQLModel session used for database queries.
 
+        Returns:
+            dict(str, Any): Dictionary containing the JWT token, token type, and expiry datetime.
 
-async def login_existing_user(login_request: LoginRequest, db: Session) -> dict[str, Any]:
-    """
-    Authenticate an existing user and generate a JWT access token.
+        Raises:
+            HTTPException: 401 Unauthorized if the email does not exist or password is incorrect.
+            HTTPException: 403 Forbidden if the user account is not verified.
+        """
+        stmt = select(User).where(User.email == login_request.email)
+        user = db.exec(stmt).one_or_none()
 
-    Queries the database for a user matching the provided email. Verifies the provided password 
-    against the stored hashed password. Checks if the user account is verified. If authentication 
-    is successful, generates a JWT token with an expiration time.
+        if not user or not verify_password(login_request.password, user.hashed_password):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid login credentials")
 
-    Args:
-        login_request (LoginRequest): Pydantic model containing user email and password.
-        db (Session): SQLModel session used for database queries.
+        if not user.is_verified:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                                detail="Account is unverified, kindly verify your email")
 
-    Returns:
-        dict(str, Any): Dictionary containing the JWT token, token type, and expiry datetime.
+        expire = datetime.now(timezone.utc) + \
+            timedelta(seconds=settings.JWT_EXPIRATION_TIME)
+        access_token = create_access_token({"sub": user.email})
 
-    Raises:
-        HTTPException: 401 Unauthorized if the email does not exist or password is incorrect.
-        HTTPException: 403 Forbidden if the user account is not verified.
-    """
-    stmt = select(User).where(User.email == login_request.email)
-    user = db.exec(stmt).one_or_none()
+        return {
+            "token": access_token,
+            "type": "bearer",
+            "expiry": expire,
+        }
 
-    if not user or not verify_password(login_request.password, user.hashed_password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid login credentials")
+    @staticmethod
+    async def resend_verification_email(request: auth_schemas.ResendVerificationEmailRequest, db: Session) -> None:
+        """
+        Generate and send a new email verification code to the user.
 
-    if not user.is_verified:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                            detail="Account is unverified, kindly verify your email")
+        Creates a new verification code and dispatches it to the specified email address.
 
-    expire = datetime.now(timezone.utc) + \
-        timedelta(seconds=settings.JWT_EXPIRATION_TIME)
-    access_token = create_access_token({"sub": user.email})
+        Args:
+            request (ResendVerificationEmailRequest): Pydantic model containing the user's email.
+            db (Session): SQLModel session used for potential database interactions.
+        """
+        stmt = select(User).where(User.email == request.email)
+        existing_user = db.exec(stmt).one_or_none()
 
-    return {
-        "token": access_token,
-        "type": "bearer",
-        "expiry": expire,
-    }
+        if not existing_user:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail="Account does not exist")
 
+        if existing_user.is_verified:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                                detail="Account is already verified")
 
-async def resend_verification_email_to_user(request: ResendVerificationEmailRequest, db: Session) -> None:
-    """
-    Generate and send a new email verification code to the user.
+        code = AuthService.generate_verification_code()
+        expires_at = datetime.now(timezone.utc) + \
+            timedelta(minutes=10)  # Valid for 10 minutes
 
-    Creates a new verification code and dispatches it to the specified email address.
+        existing_user.verification_code = code
+        existing_user.verification_code_expires_at = expires_at
+        existing_user.updated_at = datetime.now(timezone.utc)
 
-    Args:
-        request (ResendVerificationEmailRequest): Pydantic model containing the user's email.
-        db (Session): SQLModel session used for potential database interactions.
-    """
-    stmt = select(User).where(User.email == request.email)
-    existing_user = db.exec(stmt).one_or_none()
+        db.add(existing_user)
+        db.commit()
 
-    if not existing_user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail="Account does not exist")
+        await EmailService.send_verification_email(email_to=existing_user.email, code=code)
 
-    if existing_user.is_verified:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                            detail="Account is already verified")
+    @staticmethod
+    async def request_password_reset(request: auth_schemas.PasswordResetLinkRequest, db: Session) -> None:
+        """
+        Send a password reset email with a reset link to the user.
 
-    code = generate_verification_code()
-    expires_at = datetime.now(timezone.utc) + \
-        timedelta(minutes=10)  # Valid for 10 minutes
+        Generates a password reset token, then sends a reset email that includes a reset link based on the generated token to the specified email address.
 
-    existing_user.verification_code = code
-    existing_user.verification_code_expires_at = expires_at
-    existing_user.updated_at = datetime.now(timezone.utc)
+        Args:
+            request (PasswordResetLinkRequest): Pydantic model containing the user's email.
+            db (Session): SQLModel session used for potential database interactions.
+        """
+        stmt = select(User).where(User.email == request.email)
+        existing_user = db.exec(stmt).one_or_none()
 
-    db.add(existing_user)
-    db.commit()
+        if not existing_user or not existing_user.is_verified:
+            ...  # To prevent email enumeration attack, do nothing
+        reset_token = create_password_reset_token(request.email)
 
-    await es.send_verification_email(email_to=existing_user.email, code=code)
+        await EmailService.send_password_reset_email(email_to=existing_user.email, reset_token=reset_token)
+
+    @staticmethod
+    async def reset_user_password(token: str, new_password: str, db: Session) -> None:
+        """
+        Send a password reset email with a reset link to the user.
+
+        Generates a password reset token, then sends a reset email that includes a reset link based on the generated token to the specified email address.
+
+        Args:
+            token: A JWT for password reset.
+            new_password: New user's password.
+            db (Session): SQLModel session used for potential database interactions.
+        """
+        payload = decode_token(token)
+        user_email = payload.get("sub")
+
+        stmt = select(User).where(User.email == user_email)
+        existing_user = db.exec(stmt).one_or_none()
+
+        hashed_password = hash_password(new_password)
+
+        now = datetime.now(timezone.utc)
+        existing_user.hashed_password = hashed_password  # type: ignore
+        existing_user.updated_at = now  # type: ignore
+
+        # User-friendly time format
+        reset_time = now.strftime('%Y-%m-%d %H:%M:%S UTC')
+
+        db.add(existing_user)
+        db.commit()
+
+        await EmailService.send_password_reset_notification_email(email_to=existing_user.email, reset_time=reset_time)
