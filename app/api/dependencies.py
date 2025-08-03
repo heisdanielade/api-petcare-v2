f02960890b36185ec39076b263c4e40ea84c6d5d
heisdanielade <heisdanielade@gmail.com>
2025-08-03 20:53:45 +0200

    update: added password reset jwt method

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -5,13 +5,13 @@ from fastapi.security import OAuth2PasswordBearer
 from sqlmodel import Session, select
 from app.db.session import get_session
 from app.models.user import User
-from app.core.jwt import decode_access_token
+from app.core.jwt import decode_token
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 
 def get_current_user(token: str = Depends(oauth2_scheme), session: Session = Depends(get_session)) -> User:
-    payload = decode_access_token(token)
+    payload = decode_token(token)
     user_email = payload.get("sub")
     if not user_email:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
--- app/core/jwt.py
+++ app/core/jwt.py
@@ -11,22 +11,47 @@ from app.core.config import settings
 ALGORITHM = "HS256"
 
 
-def decode_access_token(token: str) -> dict[str, Any]:
+def decode_token(token: str) -> dict[str, Any]:
+    """Decode JWT token."""
     try:
-        payload = jwt.decode(token, settings.JWT_SECRET_KEY,  # type: ignore
-                             algorithms=[ALGORITHM])
+        payload = jwt.decode(
+            token, settings.JWT_SECRET_KEY,  # type: ignore
+            algorithms=[ALGORITHM])
         return payload
     except JWTError as e:
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Could not validate credentials"
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Could not validate token"
         ) from e
 
 
 def create_access_token(data: dict[str, Union[str, int]]):
+    """
+    Create login access token.
+    Valid for 3 days.
+    """
     to_encode = data.copy()
     expire = datetime.now(timezone.utc) + \
         timedelta(seconds=settings.JWT_EXPIRATION_TIME)
-    to_encode.update({"exp": expire})
+    to_encode.update({"exp": expire})  # type: ignore
+    return jwt.encode(
+        to_encode, settings.JWT_SECRET_KEY,  # type: ignore
+        algorithm=ALGORITHM)
 
-    return jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=ALGORITHM) # type: ignore
+
+def create_password_reset_token(email: str):
+    """
+    Create Password reset token.
+    Valid for 15 minutes.
+    """
+    expire = datetime.now(timezone.utc) + \
+        timedelta(minutes=15)  # Valid for 15 minutes
+    to_encode = {
+        "sub": email,
+        "exp": expire,
+        "type": "password_reset"
+    }
+
+    return jwt.encode(
+        to_encode, settings.JWT_SECRET_KEY,  # type: ignore
+        algorithm=ALGORITHM)
