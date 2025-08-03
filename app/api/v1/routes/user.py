6ad0524eb7ae414be1bcd5a3acf9c3d70ece109a
heisdanielade <heisdanielade@gmail.com>
2025-08-03 20:57:10 +0200

    refactor: moved methods to UserService class

--- app/api/v1/routes/user.py
+++ app/api/v1/routes/user.py
@@ -7,7 +7,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from app.models.user import User
 from app.utils.response import standard_response
 from app.api.dependencies import get_current_user
-from app.services.user_service import get_current_user_details
+from app.services.user_service import UserService
 
 router = APIRouter()
 
@@ -25,7 +25,7 @@ async def user_info(current_user: User = Depends(get_current_user)) -> dict[str,
     Returns:
         dict(str, Any): Success message with the user's profile details.
     """
-    response = await get_current_user_details(current_user=current_user)
+    response = await UserService.get_current_user_details(current_user=current_user)
 
     return standard_response(
         status="success",
