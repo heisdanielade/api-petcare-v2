125912ee908d651de0f29e7b8419c18964b04a89
heisdanielade <heisdanielade@gmail.com>
2025-08-08 00:35:39 +0200

    update: health endpoint now includes DB check to make sure DB connection is active

--- app/api/v1/routes/health.py
+++ app/api/v1/routes/health.py
@@ -2,15 +2,27 @@
 
 from typing import Any
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, HTTPException, Depends, status
+from sqlalchemy import text
+from sqlalchemy.exc import SQLAlchemyError
 
 from app.api.dependencies import check_verified_user
 from app.utils.response import standard_response
-
+from app.db.session import engine
 router = APIRouter()
 
 
 # @router.get("/", dependencies=[Depends(check_verified_user)])
 @router.get("/")
 def health_check() -> dict[str, Any]:
+    try:
+        with engine.connect() as conn:
+            conn.execute(text("SELECT 1"))
+    except SQLAlchemyError as e:
+        # TODO: add logging
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail="Service unavailable"
+        ) from e
+
     return standard_response("success", "All services are active.")
